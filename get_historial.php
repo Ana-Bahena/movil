af9119c93e0236c0b2e539054f3632bf0cbb02f8
Ana-Bahena <2022371113@uteq.edu.mx>
2025-04-03 16:50:00 -0600

    Update get_historial.php

--- get_historial.php
+++ get_historial.php
@@ -4,10 +4,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 
-$host = "localhost";  
-$user = "root";       
-$password = "";       
-$database = "freshbox";
+$host = "134.199.211.202";  
+$user = "ana";       
+$password = "ana123";       
+$database = "freshBox"; 
 
 $conn = new mysqli($host, $user, $password, $database);
 
